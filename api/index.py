e7ad91b5211b390a07a5fd98ddd58a2b46149be9
Marvellous Anaeto <marvythedev001@gmail.com>
2026-04-18 09:15:27 +0100

    Fixed deployment related bugs

--- api/index.py
+++ api/index.py
@@ -3,7 +3,7 @@ from mangum import Mangum
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
-    "name_classifier_api.settings"
+    "name_classifier_api_v2.settings"
 )
 
 from django.core.asgi import get_asgi_application
