aa3ee5d136a475cdd46dad24e1017527fad35073
Marvellous Anaeto <marvythedev001@gmail.com>
2026-04-18 10:33:41 +0100

    Fixed

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path("profiles", list_profiles),
-    path("profiles/", create_profile),
+    path("profiles", create_profile),
     path("profiles/<uuid:id>", get_profile),
     path("profiles/<uuid:id>/delete", delete_profile),
 ]
