716ef1f872a60a0e501c535ec28a274e24676436
Marvellous Anaeto <marvythedev001@gmail.com>
2026-04-18 11:33:38 +0100

    Almost done

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("profiles", list_profiles),
-    path("profiles", create_profile),
+    path("profiles", list_profiles, name="list_profile"),
+    path("profiles", create_profile, name="create_profile"),
     path("profiles/<uuid:id>", get_profile),
     path("profiles/<uuid:id>/delete", delete_profile),
 ]
